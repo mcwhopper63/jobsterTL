dab3e9066f95b63b2d8339f8757c6ea7fa90f443
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2023-06-30 12:07:29 -0400

    added toastify to register page

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { createRoot } from 'react-dom/client';
 import 'normalize.css'
 import './index.css';
 import App from './App';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<App tab='home' />)
+root.render(
+    <Provider store={store}>
+        <App tab='home' />
+    </Provider>
+)
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
+import { toast } from 'react-toastify';
 
 const initialState = {
   name: '',
@@ -13,28 +14,41 @@ const Register = () => {
   const [values, setValues] = useState(initialState)
 
   const handleChange = (e) => {
-    console.log(e.target)
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({...values, [name]:value})
+    console.log(`${name}:${value}`)  
   }
   
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log(e.target)
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('pls fill out all forms')
+    } 
+  }
+
+  const toggleMember = () => {
+    setValues({...values, isMember: !values.isMember})
   }
 
   return (
     <Wrapper className='full-page'>
       <form action="" className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{ values.isMember ? 'login' : 'signup'}</h3>
         {/* name field */}
-        <FormRow type="text" name="name" value={values.name} handleChange={handleChange}/>
+        {!values.isMember && (<FormRow type="text" name="name" value={values.name} handleChange={handleChange}/>)}        
         {/* email field */}
         <FormRow type="email" name="email" value={values.email} handleChange={handleChange}/>
         {/* password field */}
         <FormRow type="password" name="password" value={values.password} handleChange={handleChange}/>
         <button className='btn btn-block' type='submit'>submit</button>
+        { values.isMember ? (<p>Not a member yet?</p>) : (<p>Already a member?</p>)}
+        <button type="button" onClick={toggleMember} className="member-btn">
+          { values.isMember? "Sign Up" : "Log In"}
+        </button>
       </form>
-
     </Wrapper>
   )
 }
