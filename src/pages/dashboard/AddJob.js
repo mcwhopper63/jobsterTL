1410cece372b707d7568cc6c4cd7da16de2e01b3
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2023-07-13 15:24:21 -0400

    added allJobsSlice.js

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -2,7 +2,8 @@ import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch, } from 'react-redux';
 import { toast } from 'react-toastify';
-import { handleChange, clearValues } from '../../features/job/jobSlice';
+import { handleChange, clearValues, createJob } from '../../features/job/jobSlice';
+import { useEffect } from 'react';
 
 const AddJob = () => {
   const { 
@@ -16,7 +17,15 @@ const AddJob = () => {
     statusOptions,
     isEditing,
     editJobId,
-  } = useSelector((store) => store.job)  
+  } = useSelector((store) => store.job)
+
+  const { user } = useSelector((store)=> store.user)
+
+  useEffect(()=>{
+    if (!isEditing) {
+      dispatch(handleChange({name: 'jobLocation', value: user.location }));
+    }
+  }, [])
   
   const dispatch = useDispatch();
   
@@ -27,6 +36,8 @@ const AddJob = () => {
       toast.error('Please Fill Out All Fields');
       return;
     }
+    
+    dispatch(createJob({position, company, jobLocation, jobType, status}))
   }
 
   const handleJobInput = (e) => {
